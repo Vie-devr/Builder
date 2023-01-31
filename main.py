6942c2c407b672db1951b7eaca91e990046cad72
Pavel <vie.devr@gmail.com>
2023-01-31 01:28:00 +0100

    All now in one file, rewrited get_included_files_recursive function

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 import subprocess
 import sys
 import os
-import utils
+import re
+
+source_files_ext = ''
+
+def get_included_files_recursive(file, visited=[]):
+    files = []
+    with open(file, 'r') as f: lines = f.readlines()
+
+    for line in lines:
+        match = re.search(r'^\s*#include\s+"(.+)"', line) # Search for include directive
+        if not match:
+            continue
+
+        included_file = match.group(1) # Getting included file
+        included_file_path = os.path.join(os.path.dirname(file), included_file) # And path to it, relative to main.py
+
+        if included_file_path in visited:
+            continue
+
+        visited.append(included_file_path)
+        files += [included_file_path] + get_included_files_recursive(included_file_path, visited)
+
+        
+        if os.path.splitext(included_file)[1] in ['.h', '.hpp']: # If file is header
+            source_file = included_file.replace('.hpp', source_files_ext).replace('.h', source_files_ext) # Getting its source file
+            source_file_path = os.path.join(os.path.dirname(file), source_file) # And path to it
+            
+            try:
+                files += [source_file_path] + get_included_files_recursive(included_file_path, visited)
+            except:
+                print(f'File {source_file_path} not found.')
+                sys.exit(1)
+
+    return files
 
 def build(main_file, output, compiler_flags):
     main_file_basename, main_file_ext = os.path.splitext(main_file)
     main_file_basename = os.path.basename(main_file_basename)
 
-    files = utils.get_included_files_recursive(main_file) + [main_file]
+    global source_files_ext
+    source_files_ext = main_file_ext
+
+    files = [main_file] + get_included_files_recursive(main_file) # Files list contains main file and all dependencies for it
 
     if not output:
         output = main_file_basename
 
-    compiler = 'gcc' if main_file_ext == '.c' else 'g++' if main_file_ext == '.cpp' else ''
+    compiler = 'gcc' if main_file_ext == '.c' else 'g++' if main_file_ext == '.cpp' else '' # What compiler should we use?
     if not compiler:
-        raise Exception('Wrong file extension!')
+        raise Exception('File you\'re passed is not C or C++ source file!')
 
     cmd = [compiler] + compiler_flags + files + ['-o', output]
     
     try:
         subprocess.check_call(cmd)
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError as e: # Compiler returned error
         print(f'Build failed with return code: {e.returncode}')
         sys.exit(1)
     else:
@@ -35,7 +71,7 @@ def explain_usage():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2: # Args not passed
         explain_usage()
         sys.exit(1)
 
